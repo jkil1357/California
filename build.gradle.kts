b4e9e89572c66dc9400550978468d4f9a9130a40
[jkil1357 <[jkil1357@naver.com]>
2025-04-28 20:08:44 +0900

    commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.nanoit.agent"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation ("org.mariadb.jdbc:mariadb-java-client:3.1.4") // 최신 버전 기준
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
