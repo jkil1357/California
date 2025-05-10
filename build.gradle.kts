799c3881acbcc9e702052ad36953aabcd1947311
이민규 <kyu462755@gmail.com>
2025-05-11 06:52:57 +0900

    마리아DB와 연동까지 성공 오류는 아직 테이블 생성이 안되어있음

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,14 +21,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.4")
+    //runtimeOnly("com.h2database:h2")
 
     annotationProcessor("org.projectlombok:lombok")
     implementation("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
