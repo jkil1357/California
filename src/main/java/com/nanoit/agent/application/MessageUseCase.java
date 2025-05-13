41cc48bb43727dc6b3aa465110083c7c2f9b7802
변혜성 <67037096+ShutupWolcott@users.noreply.github.com>
2025-05-14 00:52:38 +0900

    Update MessageUseCase.java

--- src/main/java/com/nanoit/agent/application/MessageUseCase.java
+++ src/main/java/com/nanoit/agent/application/MessageUseCase.java
@@ -1,7 +1,12 @@
 package com.nanoit.agent.application;
 
+import com.nanoit.agent.core.ValidateMessage;
 import com.nanoit.agent.domain.Message;
-import com.nanoit.agent.domain.ShortMessage;
+import com.nanoit.agent.domain.MessageStatus;
+import com.nanoit.agent.port.input.MessageInputPort;
+import com.nanoit.agent.port.output.KtTransportOutputPort;
+import com.nanoit.agent.port.output.NanoitTransportOutputPort;
+import com.nanoit.agent.port.output.PersistenceOutputPort;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +17,18 @@ public class MessageUseCase implements MessageInputPort {
     private final KtTransportOutputPort ktTransportOutputPort;
     private final NanoitTransportOutputPort nanoitTransportOutputPort;
     private final PersistenceOutputPort persistenceOutputPort;
+    private final ValidateMessage validateMessage;
 
-    public MessageUseCase(KtTransportOutputPort ktTransportOutputPort, NanoitTransportOutputPort nanoitTransportOutputPort, PersistenceOutputPort persistenceOutputPort) {
+    public MessageUseCase(
+            KtTransportOutputPort ktTransportOutputPort,
+            NanoitTransportOutputPort nanoitTransportOutputPort,
+            PersistenceOutputPort persistenceOutputPort,
+            ValidateMessage validateMessage
+    ) {
         this.ktTransportOutputPort = ktTransportOutputPort;
         this.nanoitTransportOutputPort = nanoitTransportOutputPort;
         this.persistenceOutputPort = persistenceOutputPort;
+        this.validateMessage = validateMessage;
     }
 
     /**
@@ -25,39 +37,42 @@ public class MessageUseCase implements MessageInputPort {
      */
     @Override
     public void send(Message message) {
-        log.info("{}", message);
-        // 비즈니스 로직 1 - 수신번호가 전화번호가 맞는지
-        // 비즈니스 로직 2 - 발신번호가 전화번호가 맞는지
-        // 비즈니스 로직 3 - 메시지 내용이 있는지
-        // 비즈니스 로직
-        // 비즈니스 로직
-        // 비즈니스 로직
-        // 비즈니스 로직
+        log.info("메시지 수신: {}", message);
 
-        if (message instanceof ShortMessage shortMessage) {
-            if (shortMessage.to().equalsIgnoreCase("KT")) {
-                // kt로 전송하고 싶을때
-                if (ktTransportOutputPort.send(message)) {
-                    // 전송 성공
-                    shortMessage.withStatus("OK");
-                    persistenceOutputPort.update(shortMessage);
-                } else {
-                    // 전송 실패
-                    shortMessage.withStatus("FAIL");
-                    persistenceOutputPort.update(message);
+        try {
+            //유효성 검사
+            validateMessage.validate(message);
+
+            //분기 처리
+            switch (message.to()) {
+                case "KT" -> {
+                    ktTransportOutputPort.send(message);
+                    message.setStatus(MessageStatus.OK);
+                    log.info("KT 전송 성공");
                 }
-            } else if (shortMessage.to().equalsIgnoreCase("NANOIT")) {
-                // 나노아이티로 전송하고 싶을때
-                if (nanoitTransportOutputPort.send(message)) {
-                    // 전송 성공
-                    shortMessage.withStatus("OK");
-                    persistenceOutputPort.update(shortMessage);
-                } else {
-                    // 전송 실패
-                    shortMessage.withStatus("FAIL");
-                    persistenceOutputPort.update(message);
+                case "NANOIT" -> {
+                    nanoitTransportOutputPort.send(message);
+                    message.setStatus(MessageStatus.OK);
+                    log.info("NANOIT 전송 성공");
+                }
+                default -> {
+                    log.error("지원하지 않는 전송 대상: {}", message.to());
+                    message.setStatus(MessageStatus.FAIL);
                 }
             }
+
+        } catch (Exception e) {
+            
+            message.setStatus(MessageStatus.FAIL);
+            log.error("메시지 전송 실패: {}", e.getMessage(), e);
+        }
+
+        //DB 저장
+        try {
+            persistenceOutputPort.update(message);
+            log.info("메시지 상태 저장 완료: {}", message.getStatus());
+        } catch (Exception e) {
+            log.error("메시지 상태 저장 실패: {}", e.getMessage(), e);
         }
     }
 }
