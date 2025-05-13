34aa32915d63a22faf5716a197d9ac457fd8997c
변혜성 <67037096+ShutupWolcott@users.noreply.github.com>
2025-05-14 01:01:35 +0900

    Update Message.java

--- src/main/java/com/nanoit/agent/domain/Message.java
+++ src/main/java/com/nanoit/agent/domain/Message.java
@@ -2,11 +2,14 @@ package com.nanoit.agent.domain;
 
 public interface Message {
 
-    String id();
+    
+    String id();              
+    String receiveNumber();     
+    String callbackNumber();   
+    String message();           
 
-    String receiveNumber();
-
-    String callbackNumber();
-
-    String message();
+    // 분기 처리와 상태 저장을 위한 필수 메서드
+    String to();                              // KT, NANOIT
+    void setStatus(MessageStatus status);     // OK, FAIL
+    MessageStatus getStatus();                // 상태 조회
 }
